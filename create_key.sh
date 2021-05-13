c53f5aa8732fc51450281e49ed8c96fe4e8c06fe
Deepak Yadav <60976631+DEALTALFA@users.noreply.github.com>
2021-05-13 18:33:23 +0530

    vica-versa key convertion
    PEM to PPK
    PPK to PEM

--- create_key.sh
+++ create_key.sh
@@ -1,2 +1,7 @@
 #create private and pub key
 ssh-keygen -t rsa -m pem -f private.pem
+#pem to ppk
+puttygen private.pem -o private.ppk -O private
+#ppk to pem
+puttygen private.ppk -O private-openssh -o pemkey.pem
+
