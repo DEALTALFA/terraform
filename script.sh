4ddf76aa1502ea63a05e855d7ec3e5cd86d8e0da
Deepak Yadav <60976631+DEALTALFA@users.noreply.github.com>
2021-05-14 11:12:03 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,6 +0,0 @@
-sudo mkfs.ext4 /dev/xvdb
-sudo mount /dev/xvdb /var/www/html
-sudo yum install git -y
-sudo git clone https://github.com/DEALTALFA/test2.git /var/www/html
-sudo yum install httpd -y
-sudo yum enable httpd --now     
