40c251d2a025c85a18df5862ad7ee35fa804325a
Deepak Yadav <alfo7742@gmail.com>
2021-05-14 13:53:11 +0530

    making a script with else if

--- create_key.sh
+++ create_key.sh
@@ -1,7 +1,19 @@
-#create private and pub key
-ssh-keygen -t rsa -m pem -f private.pem
-#pem to ppk
-puttygen private.pem -o private.ppk -O private
-#ppk to pem
-puttygen private.ppk -O private-openssh -o pemkey.pem
 
+if [ "$(uname -o)" == "GNU/Linux" ]
+then
+	#create private and pub key
+	ssh-keygen -t rsa -m pem -f "private"
+else
+	echo "Not a Linux System"
+fi
+
+
+: '
+	covert pem to ppk then run below command
+	puttygen private -o private.ppk -O private   
+	
+	covert ppk to pem .run below command
+	puttygen private.ppk -O private-openssh -o pemkey.pem
+
+'	
+# if want to run the command copy and paste in your terminal
